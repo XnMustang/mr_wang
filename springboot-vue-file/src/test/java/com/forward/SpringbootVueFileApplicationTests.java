65d02ad94a9ec325553cbbfaac85a6cebfa209a1
xi_nian <1617171230@qq.com>
2020-12-01 14:47:27 +0800

    初始化-0.10

---
+++ springboot-vue-file/src/test/java/com/forward/SpringbootVueFileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forward;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootVueFileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
